68509560837dafe6fcff8ce1cd315d527cfbdadc
xionghl0314 <xionghl0314@thundersoft.com>
2019-02-25 15:23:30 +0800

    Add scroller.

--- app/src/main/java/com/zhengzs/rotate3dview/Rotate3dView.java
+++ app/src/main/java/com/zhengzs/rotate3dview/Rotate3dView.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.graphics.Camera;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
+import android.print.PrinterId;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Scroller;
 
 public class Rotate3dView extends ViewGroup {
     private int mWidth;
@@ -15,15 +18,21 @@ public class Rotate3dView extends ViewGroup {
     private float mDown;
     private Camera mCamera;
     private Matrix mMatrix;
+    private Scroller mScroller;
+    private Context mContext;
+    private VelocityTracker mVelocityTracker;
 
     public Rotate3dView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mContext = context;
         init();
     }
 
     private void init() {
         mMatrix = new Matrix();
         mCamera = new Camera();
+        mScroller = new Scroller(mContext);
+        mVelocityTracker = VelocityTracker.obtain();
     }
 
     @Override
@@ -52,7 +61,7 @@ public class Rotate3dView extends ViewGroup {
     @Override
     protected void dispatchDraw(Canvas canvas) {
         for (int i = 0; i < getChildCount(); i++) {
-            drawPage(canvas,i,getDrawingTime());
+            drawPage(canvas, i, getDrawingTime());
         }
     }
 
@@ -81,25 +90,51 @@ public class Rotate3dView extends ViewGroup {
     }
 
     @Override
+    public void computeScroll() {
+        if (mScroller.computeScrollOffset()) {
+            scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+            postInvalidate();
+        }
+    }
+
+    @Override
     public boolean onTouchEvent(MotionEvent event) {
+        mVelocityTracker.addMovement(event);
+        mVelocityTracker.computeCurrentVelocity(1000);
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 mDown = event.getY();
+                if (!mScroller.isFinished()) {
+                    mScroller.setFinalY(mScroller.getCurrY());
+                    mScroller.abortAnimation();
+                    scrollTo(0, getScrollY());
+                }
                 break;
             case MotionEvent.ACTION_MOVE:
                 float delta = mDown - event.getY();
                 mDown = event.getY();
                 scrollBy(0, (int) delta);
-                if (getScrollY() < 0) {
-                    addPrePage();
-                    break;
-                }
-                if (getScrollY() > (getChildCount() - 1) * mHeight) {
-                    addNextPage();
-                    break;
+                if(mScroller.isFinished()) {
+                    if (getScrollY() < 0) {
+                        addPrePage();
+                        break;
+                    }
+                    if (getScrollY() > (getChildCount() - 1) * mHeight) {
+                        addNextPage();
+                        break;
+                    }
                 }
                 break;
             case MotionEvent.ACTION_UP:
+                int scrollDelta = Math.abs(getScrollY()) % mHeight;
+                if (mVelocityTracker.getYVelocity() > 300) {
+                    movePage(-scrollDelta);
+                } else if (mVelocityTracker.getYVelocity() < -300) {
+                    movePage(mHeight - scrollDelta);
+                } else {
+                    int scrollValue = scrollDelta > mHeight / 2 ? mHeight - scrollDelta : -scrollDelta;
+                    movePage(scrollValue);
+                }
                 break;
         }
         return true;
@@ -117,5 +152,10 @@ public class Rotate3dView extends ViewGroup {
         removeViewAt(0);
         addView(childView, getChildCount());
         scrollBy(0, -mHeight);
+    }
+
+    private void movePage(int scrollValue) {
+        mScroller.startScroll(0, getScrollY(), 0, scrollValue, 1000);
+        invalidate();
     }
 }
